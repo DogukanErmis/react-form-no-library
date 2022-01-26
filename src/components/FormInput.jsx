fbcc97426d64754aa8816f0b8d5a730bc85e59c1
dogukanermis <ermisd+github@pm.me>
2022-01-26 13:12:23 +0100

    create FormInput component

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import "./FormInput.css";
+
+const FormInput = () => {
+  return (
+    <div className="formInput">
+      <label htmlFor="">Username</label>
+      <input />
+    </div>
+  );
+};
+
+export default FormInput;
