c61fdf02a8daa5b8b1543ba1d33890f8812cf7d5
dogukanermis <ermisd+github@pm.me>
2022-01-26 15:47:56 +0100

    display error message when input is out of focus

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,23 @@
 import "./FormInput.css";
+import { useState } from "react";
 
 const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
   const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
       <label htmlFor="">{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
       <span>{errorMessage}</span>
     </div>
   );
